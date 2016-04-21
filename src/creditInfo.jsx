522d7c8d128dc1a1ded74a9bb845bcc68f2ee099
rikin88 <rikin.asher@gmail.com>
2016-04-21 12:12:26 -0500

    added pie chart

--- src/creditInfo.jsx
+++ src/creditInfo.jsx
@@ -1,8 +1,34 @@
 var React = require('react');
+var PieChart = require('react-chartjs').Pie;
+
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      "chartData": [
+        {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+      ],
+      "chartOptions": []
+    }
+  },
   render: function() {
-
     return (
     <div className="col-sm-6">
       <div className={"panel panel-info "+ this.props.styleInfo}>
@@ -10,14 +36,8 @@ module.exports = React.createClass({
           <h3 className="panel-title">Credit Summary</h3>
         </div>
         <div className="panel-body">
-    Credit
-          <ul>
-          
-            {this.props.creditInfo.map(function(account) {
-              return <ListItemWrapper data={account.CreditSummary.CreditLimit}/>;
-            })
-            }
-          </ul>
+          Credit
+          <PieChart data={this.state.chartData} options={this.state.chartOptions} />
 
       </div>
       </div>
